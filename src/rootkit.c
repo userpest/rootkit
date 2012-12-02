c5531e7234c363edc35fcb38d55b77e866016707
Michal Antonczak <uzyszkodnik.reg@gmail.com>
2012-12-02 19:40:46 +0100

    working module

--- src/rootkit.c
+++ src/rootkit.c
@@ -1,1 +1,21 @@
-#include <
+#include <linux/version.h>
+#include <linux/module.h>
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Michal Antonczak");
+MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("totally harmless module");
+
+static int __init m_init(void){
+	printk(KERN_INFO "rkit module loaded");
+	return 0; 
+
+}
+
+static void __exit m_exit(void){
+	printk(KERN_INFO "unloading");
+	return;
+}
+module_init(m_init);
+module_exit(m_exit);
